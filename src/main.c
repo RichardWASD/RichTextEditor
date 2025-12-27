3aa5ff3894ecb53e9f4863ff08e1744d575b2813
RichardWASD <Richard.hernandez@stonybrook.edu>
2025-12-27 05:49:24 +0000

    Correctly writing to files (Initial development)

--- src/main.c
+++ src/main.c
@@ -2,6 +2,24 @@
 
 int main (){
 
-    printf("Hello World!\n");
+    FILE *file_pointer ; 
+    char input_text[100];
+
+    printf("Rich Editor!\n");
+    
+    file_pointer = fopen("output.txt","w"); // Pointer creates the output file 
+
+    while(file_pointer == NULL){
+        exit(1);
+    }
+
+    fgets(input_text, sizeof(input_text),stdin); // for entire line of text (including whitespace!)
+    fprintf(file_pointer, "%s", input_text); // writes to file 
+
+
+
+
+    
+    fclose(file_pointer);
     return 0;
 }
